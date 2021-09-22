9c85ff80574b9fa21897dd77a7f633134bde4c26
pawan-kbih <91180965+pawan-kbih@users.noreply.github.com>
2021-09-22 12:45:19 +0530

    Add files via upload

---
+++ 02_strings_slicing.py
@@ -0,0 +1,17 @@
+# greetings = "Good Night, "
+# name = "Namita"
+# c= greetings + name #-->Concatenating two strings
+# print(c)
+# name= "Namita"
+# print(name[3])# -->> Strings indexing
+# name[3]= "P" # --> does't work in reverse to replace the strings
+# i.e. we can access the character under strings but can't change it.
+# name= "Namita"
+# print(name[0:3]) #-->> Strings slicing
+# print(name[1:4]) #-->> Strings slicing
+# print(name[:4])# is same as print(name[0:4])
+# print(name[0:])# is same as print(name[0:last digit position])
+# print(name[1:])# is same as print(name[1:last digit position(5)])
+# print (name[-1]) # this will give last value of stings
+name= "Namitakumari"
+print(name[0: :2]) # Slicing with skip value
